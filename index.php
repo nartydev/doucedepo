84128309a041e91f2829fb217c53890e5fc01e57
nartydev <narty.dev@gmail.com>
2018-02-12 10:15:45 +0100

    start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>DouceDePo</title>
+	<meta charset="utf-8"/>
+	<meta description="DouceDePo"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
