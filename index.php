87a905791d7ea599a67f7cbbacae902fe9fa998f
nartydev <narty.dev@gmail.com>
2018-02-13 13:41:38 +0100

    texte, produit presque fini

--- index.php
+++ index.php
@@ -9,62 +9,127 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css?d=<?php echo rand(0,10000000000000000) ?>">
 </head>
 <body>
-    <header>
-        <div class="container between">
-            <a href="./index.php" title="DouceDePo"><img src="assets/img/logo-white.png" alt="DouceDePo" class="logo" /></a>
-            <nav>
-                <ul>
-                    <li><a href="/index.php">Accueil</a></li>
-                    <li><a href="/collection.php">Nos collections</a></li>
-                    <li><a href="/panier.php">Mon panier</a></li>
-                    <li class="active"><a href="/compte.php">Mon compte</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    <div class="slider">
-        <div class="one-slide active-slide" slide-number="0">
-            <div class="container content-slider">
-                <img src="assets/img/bavoir-1.png" alt="Slider 1" class="img-slider">
-                <div class="text-content-slider">
-                    <h2 class="title-slider">Linguerie pour bébé</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias voluptatem obcaecati accusantium aliquam unde eum vel aliquid sint quo autem.</p>
-                    <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
-                </div>
-                <div class="clear"></div>
+    <div class="gradient">
+        <header>
+            <div class="container between">
+                <a href="./index.php" title="DouceDePo"><img src="assets/img/logo-white.png" alt="DouceDePo" class="logo" /></a>
+                <nav>
+                    <ul>
+                        <li><a href="/index.php">Accueil</a></li>
+                        <li><a href="/collection.php">Nos collections</a></li>
+                        <li><a href="/panier.php">Mon panier</a></li>
+                        <li class="active"><a href="/compte.php">Mon compte</a></li>
+                    </ul>
+                </nav>
             </div>
-        </div>
-        <div class="one-slide" slide-number="1">
-            <div class="container content-slider">
-                <img src="assets/img/bavoir-1.png" alt="Slider 1" class="img-slider">
-                <div class="text-content-slider">
-                    <h2 class="title-slider">Linguerie pour bébé</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias voluptatem obcaecati accusantium aliquam unde eum vel aliquid sint quo autem.</p>
-                    <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
+        </header>
+        <div class="slider">
+            <div class="one-slide active-slide" slide-number="0">
+                <div class="container content-slider">
+                    <img src="assets/img/bavoir-1.png" alt="Slider 1" class="img-slider">
+                    <div class="text-content-slider">
+                        <h2 class="title-slider">UN... DEUX... TROIS MOUTONS</h2>
+                        <p>Parce que le reve est essentiel, decouvrez mes produits pour qu’il passe une bonne nuit</p>
+                        <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
+                    </div>
+                    <div class="clear"></div>
                 </div>
-                <div class="clear"></div>
-            </div>       
+            </div>
+            <div class="one-slide" slide-number="1">
+                <div class="container content-slider">
+                    <img src="assets/img/personal.png" alt="Slider 1" class="img-slider">
+                    <div class="text-content-slider">
+                        <h2 class="title-slider">Une sélection de produits personnalisables</h2>
+                        <p>Du sac à dos au coussin, une sélection d’objets peut porter le nom de votre nourisson bien aimé. Spécifiez le prénom ainsi que le motif et le coloris des caractères et vous aurez l’assurance d’avoir un objet que vous ne verrez nulle part ailleurs.</p>
+                        <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
+                    </div>
+                    <div class="clear"></div>
+                </div>       
+            </div>
+            <div class="one-slide" slide-number="2">
+                <div class="container content-slider">
+                    <img src="assets/img/bavoir-1.png" alt="Slider 1" class="img-slider">
+                    <div class="text-content-slider">
+                        <h2 class="title-slider">Linguerie pour bébé</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias voluptatem obcaecati accusantium aliquam unde eum vel aliquid sint quo autem.</p>
+                        <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
+                    </div>
+                    <div class="clear"></div>
+                </div>  
+            </div>
+            <div class="cloud"></div>
+            <ul class="selector-slider">
+                <li class="slide-switch active" slide-number="0"></li>
+                <li class="slide-switch" slide-number="1"></li>
+                <li class="slide-switch" slide-number="2"></li>
+            </ul>
         </div>
-        <div class="one-slide" slide-number="2">
-            <div class="container content-slider">
-                <img src="assets/img/bavoir-1.png" alt="Slider 1" class="img-slider">
-                <div class="text-content-slider">
-                    <h2 class="title-slider">Linguerie pour bébé</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias voluptatem obcaecati accusantium aliquam unde eum vel aliquid sint quo autem.</p>
-                    <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
-                </div>
-                <div class="clear"></div>
-            </div>  
-        </div>
-        <div class="cloud"></div>
-        <ul class="selector-slider">
-            <li class="slide-switch active" slide-number="0"></li>
-            <li class="slide-switch" slide-number="1"></li>
-            <li class="slide-switch" slide-number="2"></li>
-        </ul>
     </div>
     <section class="white">
-
+        <div class="container start">
+            <div class="col">
+                <div class="img-made-france"></div>
+                <h3>Fabrication locale</h3>
+                <div class="border-info"></div>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium beatae ad officia quis libero nostrum culpa eos totam aliquid ducimus.
+                </p>
+            </div>
+            <div class="col">
+                <div class="img-local"></div>
+                <h3>Fait à la main</h3>
+                <div class="border-info"></div>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium beatae ad officia quis libero nostrum culpa eos totam aliquid ducimus.
+                </p>
+            </div>
+            <div class="col">
+                <div class="img-quality"></div>
+                <h3>Qualité supérieur</h3>
+                <div class="border-info"></div>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium beatae ad officia quis libero nostrum culpa eos totam aliquid ducimus.
+                </p>
+            </div>
+        </div>
+        <div class="cloud-bottom"></div>
+    </section>
+    <section class="gradient-inverse">
+        <div class="container">
+            <h2 class="title-products">Nos produits</h2>
+        </div>
+        <div class="container start">
+            <div class="product">
+                <div class="black"></div>
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="desc-product">
+                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
+                    <div class="price-product">40.00€</div>
+                    <div class="speed-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, exercitationem. Lorem ipsum dolor sit amet.</div>
+                </div> 
+            </div>
+            <div class="product">
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="desc-product">
+                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
+                    <div class="price-product">40.00€</div>
+                </div> 
+            </div>
+            <div class="product">
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="desc-product">
+                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
+                    <div class="price-product">40.00€</div>
+                </div> 
+            </div>
+            <div class="product">
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="desc-product">
+                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
+                    <div class="price-product">40.00€</div>
+                </div> 
+            </div>
+        </div>
     </section>
     <script src="assets/js/app.js?d=<?php echo rand(0,10000000000000000) ?>"></script>
 </body>
