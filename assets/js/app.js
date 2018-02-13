3971246781cdfd3f68d2a753cb04590962b6dba8
nartydev <narty.dev@gmail.com>
2018-02-13 16:55:06 +0100

    ajout footer et correction détail

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,6 @@ const slide = document.querySelectorAll('.one-slide');
 
 let idSlider = 0;
 
-
 for(let i = 0; i < allSwitch.length; i++) {
     allSwitch[i].addEventListener('click', () => {
         if(allSwitch[i].getAttribute('slide-number') == slide[i].getAttribute('slide-number')) {
@@ -15,3 +14,26 @@ for(let i = 0; i < allSwitch.length; i++) {
         }
     })
 }
+
+// MENU 
+
+const header = document.querySelector('header')
+const link = document.querySelectorAll('nav ul li a')
+const borderColor = document.querySelector('.active')
+
+addEventListener("scroll", () => {
+	console.log(window.innerWidth);
+	if(pageYOffset > 10) {
+        header.classList.add('menu-scroll')
+        for(i = 0; i < link.length; i++) {
+            link[i].classList.add('color-white')
+        }
+        borderColor.style.border = "1px solid #A6A6A6"
+	} else {
+        header.classList.remove('menu-scroll')
+        for(i = 0; i < link.length; i++) {
+            link[i].classList.remove('color-white')
+        }
+        borderColor.style.border = "1px solid #FFF"        
+	}
+});
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
     <div class="gradient">
         <header>
-            <div class="container between">
+            <div class="container between resize-container">
                 <a href="./index.php" title="DouceDePo"><img src="assets/img/logo-white.png" alt="DouceDePo" class="logo" /></a>
                 <nav>
                     <ul>
@@ -29,7 +29,7 @@
                     <img src="assets/img/bavoir-1.png" alt="Slider 1" class="img-slider">
                     <div class="text-content-slider">
                         <h2 class="title-slider">UN... DEUX... TROIS MOUTONS</h2>
-                        <p>Parce que le reve est essentiel, decouvrez mes produits pour qu’il passe une bonne nuit</p>
+                        <p>Parce que le reve est essentiel, decouvrez mes produits pour qu’il passe une bonne nuit. Ma sélection d'attaches tétines et de gigoteuses lui garantiront une nuit paisible</p>
                         <a href="#" title="En savoir plus" class="button-slider">En savoir plus</a>
                     </div>
                     <div class="clear"></div>
@@ -68,11 +68,11 @@
     <section class="white">
         <div class="container start">
             <div class="col">
-                <div class="img-made-france"></div>
-                <h3>Fabrication locale</h3>
+                <div class="img-personal"></div>
+                <h3>Personnalisation</h3>
                 <div class="border-info"></div>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium beatae ad officia quis libero nostrum culpa eos totam aliquid ducimus.
+                    Sur Douce de Po c’est vous qui choisissez l’apparence de votre produit final grâce à la personnalisation. Vous voulez un cadeau unique pour une naissance ? Vous pouvez indiquer le prénom du nouveau né sur une sélection d’objets et même choisir les motifs et les coloris des caractères. 
                 </p>
             </div>
             <div class="col">
@@ -80,7 +80,7 @@
                 <h3>Fait à la main</h3>
                 <div class="border-info"></div>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium beatae ad officia quis libero nostrum culpa eos totam aliquid ducimus.
+                    Tous les produits disponibles sur le site sont fabriqués à la main chez moi, tout est donc à 100% français et artisanal.
                 </p>
             </div>
             <div class="col">
@@ -88,7 +88,7 @@
                 <h3>Qualité supérieur</h3>
                 <div class="border-info"></div>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium beatae ad officia quis libero nostrum culpa eos totam aliquid ducimus.
+                Les matériaux sont sélectionnés avec soin pour que l’objet soit durable mais surtout pour qu’il soit adapté à la peau fragile de nos bébés, tous les tissus sont composés de fibres naturelles.
                 </p>
             </div>
         </div>
@@ -96,7 +96,7 @@
     </section>
     <section class="gradient-inverse">
         <div class="container">
-            <h2 class="title-products">Nos produits</h2>
+            <h2 class="title-products">Mes produits</h2>
         </div>
         <div class="container start">
             <div class="product">
@@ -109,28 +109,66 @@
                 </div> 
             </div>
             <div class="product">
-                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="black"></div>
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/lapinou-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
                 <div class="desc-product">
-                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
-                    <div class="price-product">40.00€</div>
+                    <div class="title-product">Toudoux Lapinou Au Pays des Rêves</div>
+                    <div class="price-product">20.00€</div>
+                    <div class="speed-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, exercitationem. Lorem ipsum dolor sit amet.</div>
                 </div> 
             </div>
             <div class="product">
-                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="black"></div>
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/tapis-a-langer-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
                 <div class="desc-product">
-                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
-                    <div class="price-product">40.00€</div>
+                    <div class="title-product">Tapis à langer nomade Au Pays des Rêves</div>
+                    <div class="price-product">38.00€</div>
+                    <div class="speed-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, exercitationem. Lorem ipsum dolor sit amet.</div>
                 </div> 
             </div>
             <div class="product">
-                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/oiseau-2-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
+                <div class="black"></div>
+                <img src="http://www.doucedepo.fr/wp-content/uploads/2017/10/attache-300x300.jpg" class="img-product" alt="Oiseau musical Au Pays des Rêves"/>
                 <div class="desc-product">
-                    <div class="title-product">Oiseau musical Au Pays des Rêves</div>
-                    <div class="price-product">40.00€</div>
+                    <div class="title-product">Attache-tétine Au Pays des Rêves</div>
+                    <div class="price-product">10.00€</div>
+                    <div class="speed-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, exercitationem. Lorem ipsum dolor sit amet.</div>
                 </div> 
             </div>
+            <a href="#" class="button-white">Visiter la boutique</a>
         </div>
     </section>
+    <footer class="white dim-pad">
+        <div class="container start">
+            <div class="col-footer">
+                <div class="title-footer">contact</div>
+                <ul class="navigation">
+                    <li><i class="fa fa-map-marker pad-map"></i> 28 rue de Reims, 93160, Noisy le Grand, FRANCE</li>
+                    <li><i class="fa fa-phone pad-phone"></i> 06 25 35 87 36</li>
+                    <li><i class="fa fa-envelope"></i> doucedepo@gmail.com</li>
+
+                </ul>
+            </div>
+            <div class="col-footer">
+                <div class="title-footer">Navigation</div>
+                <ul class="navigation">
+                    <li><i class="fa fa-angle-right"></i> <a href="#" title="Accueil">Accueil</a></li>
+                    <li><i class="fa fa-angle-right"></i> <a href="#" title="Nos collections">CGV</a></li>
+                    <li><i class="fa fa-angle-right"></i> <a href="#" title="Nos collections">Contact</a></li>
+                    <li><i class="fa fa-angle-right"></i> <a href="#" title="Nos collections">Partenaires</a></li>
+                </ul>
+            </div>
+            <div class="col-footer">
+                <div class="title-footer">Nos réseaux sociaux</div>
+                <ul class="social-network">
+                    <li class="round fb"><i class="fa fa-facebook"></i></li>
+                    <li class="round instagram"><i class="fa fa-instagram"></i></li>
+                    <li class="round pinterest"><i class="fa fa-pinterest"></i></li>
+                    <li class="round gplus"><i class="fa fa-google-plus"></i></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
     <script src="assets/js/app.js?d=<?php echo rand(0,10000000000000000) ?>"></script>
 </body>
 </html>
